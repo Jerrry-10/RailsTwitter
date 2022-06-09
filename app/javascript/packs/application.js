495982fa84370bd8cebe1d5419addc2cd83e8feb
Jerrry-10 <Jerry.Aviles@cix.csi.cuny.edu>
2022-06-09 15:33:48 -0400

    Implement advanced login

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,13 +9,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-import 'bootstrap'
-window.jQuery = $;
-window.$ = $;
-global.$ = require('jquery')
-//= require bootstrap
-//= require jquery-ui
-//= require jquery
+import "bootstrap"
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
