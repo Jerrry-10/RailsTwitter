6e536eefd8884f58004ef661631a9ce7d36051bf
Jerrry-10 <Jerry.Aviles@cix.csi.cuny.edu>
2022-06-13 12:15:40 -0400

    heroku emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,20 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sample_app_production"
 
   config.action_mailer.perform_caching = false
-
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
   host = 'sheltered-woodland-53393.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
+  config.action_mailer.default_url_options = {host: "#{host}"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain => 'sheltered-woodland-53393.heroku.com',
+    :address => 'smtp.mailgun.org',
+    :port => 587,
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
     :enable_starttls_auto => true
-}
+    }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
