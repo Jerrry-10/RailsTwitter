c459ba584ce6c3d1da11711ce685f30c9a24e25a
Jerrry-10 <Jerry.Aviles@cix.csi.cuny.edu>
2022-06-13 09:46:15 -0400

    Add account ativation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -53,7 +53,7 @@ end
             assert_not @user.valid?
             end
             test "authenticated? should return false for a user with nil digest" do
-              assert_not @user.authenticated?('')
+              assert_not @user.authenticated?(:remember, '')
             end
           
 end
