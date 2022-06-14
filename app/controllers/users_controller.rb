72ce5ad2d4f5d0ddd25b92ee752df0d3e2056fdc
Jerrry-10 <Jerry.Aviles@cix.csi.cuny.edu>
2022-06-14 12:31:11 -0400

    add user mircoposts (needs images from AWS)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
     end
 
     def show
-    @user = User.find(params[:id])
-    
+        @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(page: params[:page])
     end
     def new
         @user = User.new
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)      
         if @user.save
-          @user.send_activation_email
-        
+            
+            @user.send_activation_email
+           # binding.pry
           flash[:info] = "Please check your email to activate your account."
           redirect_to root_url
         else
@@ -48,13 +49,7 @@ class UsersController < ApplicationController
         params.require(:user).permit(:name, :email, :password,
         :password_confirmation)
         end
-        def logged_in_user
-            unless logged_in?
-            store_location
-            flash[:danger] = "Please log in."
-            redirect_to login_url
-            end
-        end
+        
         def correct_user
             @user = User.find(params[:id])
             redirect_to(root_url) unless current_user?(@user)
