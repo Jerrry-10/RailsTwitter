5e1cfbd7ed7437047cac039e1db0b7fd95f75617
Jerrry-10 <Jerry.Aviles@cix.csi.cuny.edu>
2022-06-14 15:39:29 -0400

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -50,5 +50,14 @@ test "should redirect destroy when not logged in" do
   delete user_path(@user)
   end
   assert_redirected_to root_url
+end
+  test "should redirect following when not logged in" do
+  get following_user_path(@user)
+  assert_redirected_to login_url
   end
+  test "should redirect followers when not logged in" do
+  get followers_user_path(@user)
+  assert_redirected_to login_url
+  end
+  
 end
